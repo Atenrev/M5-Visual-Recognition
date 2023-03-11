4fe0aae16e44023eebbb51e34ee389c4f69931d5
alvaro-budria <al.fran.budria@gmail.com>
2023-03-11 19:01:43 +0100

    freeze layers

--- src/models/xception.py
+++ src/models/xception.py
@@ -6,35 +6,6 @@ from typing import Any
 from src.models.base_model import BaseModel
 
 
-def flatten_model(modules):
-    def flatten_list(_2d_list):
-        flat_list = []
-        # Iterate through the outer list
-        for element in _2d_list:
-            if type(element) is list:
-                # If the element is of type list, iterate through the sublist
-                for item in element:
-                    flat_list.append(item)
-            else:
-                flat_list.append(element)
-        return flat_list
-
-    ret = []
-    try:
-        for _, n in modules:
-            ret.append(loopthrough(n))
-    except:
-        try:
-            if str(modules._modules.items()) == "odict_items([])":
-                ret.append(modules)
-            else:
-                for _, n in modules._modules.items():
-                    ret.append(loopthrough(n))
-        except:
-            ret.append(modules)
-    return flatten_list(ret)
-
-
 class Xception(BaseModel):
 
     def __init__(self, config: Any, device: torch.device) -> None:
@@ -42,23 +13,16 @@ class Xception(BaseModel):
         self.model = timm.create_model('xception', pretrained=True, num_classes=config.num_classes)
         self.loss_function = torch.nn.CrossEntropyLoss()
 
-        ##### Freeze layers
-        target_layers = []
-        module_list = [module for module in self.model.modules()]
-        flatted_list = flatten_model(module_list)
-
+        ##### Freeze/Unfreeze Layers
+        param_list = self.model.parameters()
         assert 0 <= config.p_freeze <= 1
-        n_to_freeze = int(len(flatted_list) * config.p_freeze)
-        for count, value in enumerate(flatted_list[:n_to_freeze]):
-            print(count, value)
-            target_layers.append(value)
-            value.requires_grad = False
+        n_to_freeze = int(len(param_list) * config.p_freeze)
+        for param in self.model.parameters()[:n_to_freeze]:
+            param.requires_grad = False
+
+        for param in self.model.parameters()[n_to_freeze:]:
+            param.requires_grad = True
         #####
-        print('-'*25)
-        for count, value in enumerate(flatted_list[n_to_freeze:]):
-            print(count, value)
-            target_layers.append(value)
-            value.requires_grad = True
 
     def forward(self, image: torch.Tensor, **kwargs) -> dict:
         outputs = self.model(image)
