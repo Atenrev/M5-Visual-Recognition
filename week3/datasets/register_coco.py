643dd3d6780412af2eb5aba785c3b02ba6bb2540
AtenrevCode <atenrev@gmail.com>
2023-03-22 19:36:51 +0100

    remember

--- week3/datasets/register_coco.py
+++ week3/datasets/register_coco.py
@@ -1,2 +1,4 @@
 def register_coco_dataset(cfg):
+    cfg.DATASETS.TRAIN = ("coco_train",)
+    cfg.DATASETS.TEST = ("coco_test",)
     pass
--- week3/datasets/register_out_of_context.py
+++ week3/datasets/register_out_of_context.py
@@ -1,2 +1,4 @@
 def register_out_of_context_dataset(cfg):
+    cfg.DATASETS.TRAIN = ("out_of_context_train",) 
+    cfg.DATASETS.TEST = ("out_of_context_test",) 
     pass
