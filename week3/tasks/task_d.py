f4824c86e376431430d3186d025998a52b6c8c0a
AtenrevCode <atenrev@gmail.com>
2023-03-24 13:22:28 +0100

    detectron2_1?

--- week3/tasks/task_d.py
+++ week3/tasks/task_d.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from detectron2.config import get_cfg
-from detectron2_1.adv import DAGAttacker
+from detectron2.adv import DAGAttacker
 from detectron2.structures import pairwise_iou, Boxes
 from detectron2 import model_zoo
 import torch
