b3f5fe28d45509fa178ae17e9a5da919aca49e56
EauDeData <amolina@cvc.uab.cat>
2023-03-25 20:52:23 +0100

    i got a blank space baby

--- week3/tasks/task_d.py
+++ week3/tasks/task_d.py
@@ -1,4 +1,30 @@
+import json
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import cv2
 
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer
+
+MODELS = {
+    "mask": 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml',
+    "faster": 'COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml',
+}
+
+MODEL = MODELS['mask']
+
+def task_d(*args):
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(MODEL))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(MODEL)
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+
+    predictor = DefaultPredictor(cfg)
+    print(predictor)
 # i give up i went crazy
 
 if __name__ == '__main__': 
