643c49e711b08440d0b04647ed9e1aa1cf13e436
EauDeData <amolina@cvc.uab.cat>
2023-03-25 22:00:55 +0100

    i got a blank space baby

--- week3/tasks/task_d.py
+++ week3/tasks/task_d.py
@@ -49,7 +49,7 @@ def task_d(*args, attacked_image = './data/weird/el_bone.jpg', steps = 10, imsiz
     for step in range(steps):
 
         print(data.min(), data.max(), data.shape)
-        output = model([{'image': data}])
+        output = model([{'image': data}])[0]
         scores = output['instances'].scores
         print(scores)
         break        
