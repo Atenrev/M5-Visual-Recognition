e6606dd92350cfdda8bcf650f38ebac384b92a37
EauDeData <amolina@cvc.uab.cat>
2023-04-01 13:46:52 +0200

    el ga tito tuyo te perdio por negligencia

--- week4/vision/models.py
+++ week4/vision/models.py
@@ -33,3 +33,28 @@ class Resnet(torch.nn.Module):
         if self.norm is not None: h =  torch.nn.functional.normalize(h, p = self.norm, dim = 1)
         return h
     
+
+class VGG19(torch.nn.Module):
+    def __init__(self, norm = 2, batchnorm = True, pretrained = 'imagenet'):
+        super(VGG19, self).__init__()
+        model = torchvision.models.vgg19(pretrained=pretrained) if not batchnorm else torchvision.models.vgg19_bn(pretrained=pretrained) 
+
+        self.model = torch.nn.Sequential(*(list(model.children())[:-1]))
+        self.norm = norm
+
+    def infer(self, image):
+        # Single Numpy Array inference
+        with torch.no_grad():
+
+            return self(torch.from_numpy(image.transpose(2, 0, 1)).float().unsqueeze(0)).cpu().squeeze().view(-1).numpy()
+        
+    def __str__(self):
+        return str(self.model)
+
+
+    def forward(self, batch):
+
+        h = self.model(batch)
+        if self.norm is not None: h =  torch.nn.functional.normalize(h, p = self.norm, dim = 1)
+        return h
+    
