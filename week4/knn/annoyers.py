1873290c40a7ed79eb8fe119f23a7def393e7865
EauDeData <amolina@cvc.uab.cat>
2023-04-04 12:53:22 +0200

    sklearn annoyer implemented

--- week4/knn/annoyers.py
+++ week4/knn/annoyers.py
@@ -1,5 +1,7 @@
 import annoy
 import torch
+from sklearn.neighbors import NearestNeighbors
+import warnings
 
 from tqdm import tqdm
 
@@ -7,8 +9,10 @@ class Annoyer:
     # High performance approaximate nearest neighbors - agnostic wrapper
     # Find implementation and documentation on https://github.com/spotify/annoy
 
-    def __init__(self, model, dataset, emb_size, distance = 'angular', local = 'trees.ann') -> None:
-        
+    def __init__(self, model, dataset, emb_size = None, distance = 'angular', local = 'trees.ann') -> None:
+        assert not (emb_size is None) and isinstance(emb_size, int),\
+            f'When using Annoyer KNN emb_size must be an int. Set as None for common interface. Found: {type(emb_size)}' 
+
         self.model = model
 
         # FIXME: Dataloader assumes 1 - Batch Size
@@ -48,4 +52,39 @@ class Annoyer:
         return self.trees.get_nns_by_vector(vector, n, **kwargs)
 
                     
+class SKNNWrapper:
 
+    # Common interface for the annoyer KNN 
+    def __init__(self, model, dataset, distance = 'cosine', k = 5, **kwargs) -> None:
+        self.model = model
+
+        # FIXME: Dataloader assumes 1 - Batch Size
+        self.dataloader = dataset
+        self.trees = NearestNeighbors(n_neighbors = k, metric = distance)
+        self.state_variables = {
+            'built': False,
+        }
+    
+    
+    def fit(self):
+        if self.state_variables['built']: raise AssertionError('Cannot fit a built KNN')
+        else: self.state_variables['built'] = True
+
+        X = list()
+        for idx, (image, _) in enumerate(self.dataloader):
+            print(f'Building KNN... {idx} / {len(self.dataloader)}\t', end = '\r')
+            with torch.no_grad():
+                emb = self.model(image.float()).squeeze().cpu().numpy() # Ensure batch_size = 1
+
+            X.append(emb)
+        self.trees.fit(X)
+    
+    def load(self):
+        raise NotImplementedError('Load is not implemented for sklearn KNN')
+    
+    def retrieve_by_idx(self, *args, **kwargs):
+        raise NotImplementedError('Retrieval by ID is not implemented for sklearn KNN')
+
+    def retrieve_by_vector(self, vector, n = None, **kwargs):
+        if not (n is None): warnings.warn('SKLearn retrieval receives the K parameter on the constructor. Ignoring N kwarg...')
+        return self.trees.kneighbors([vector], **kwargs)[-1][0].tolist()
--- week4/shit_file.py
+++ week4/shit_file.py
@@ -1,23 +1,26 @@
 from vision.models import Resnet, VGG19
-from knn.annoyers import Annoyer
+from knn.annoyers import Annoyer, SKNNWrapper
 from datautils.datasets import ZippedDataloader
 
 from torch.utils.data import DataLoader
+import numpy as np
 # Shit file for unitary testing
+
+V = np.random.random(2048)
 
 model = Resnet(resnet = '101')
 dataset = ZippedDataloader('/home/adri/Pictures/zipper.zip')
 dataloader = DataLoader(dataset, batch_size = 1)
 
-annoy = Annoyer(model, dataloader, emb_size = 2048) # Works better with smaller emb_sizes però que li farem
+annoy = SKNNWrapper(model, dataloader, emb_size = 2048) # Works better with smaller emb_sizes però que li farem
 annoy.fit()
-print(annoy.retrieve_by_idx(3))
+print(annoy.retrieve_by_vector(V))
 
 
-model = VGG19()
+# model = VGG19()
 dataset = ZippedDataloader('/home/adri/Pictures/zipper.zip')
 dataloader = DataLoader(dataset, batch_size = 1)
 
-annoy = Annoyer(model, dataloader, emb_size = 512) # Works better with smaller emb_sizes però que li farem
+annoy = Annoyer(model, dataloader, emb_size = 2048) # Works better with smaller emb_sizes però que li farem
 annoy.fit()
-print(annoy.retrieve_by_idx(3))
+print(annoy.retrieve_by_vector(V))
