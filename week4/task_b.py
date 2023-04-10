879e126b2a558beeb28cf3ae77df4cbcf1b26e48
AtenrevCode <atenrev@gmail.com>
2023-04-10 14:26:17 +0200

    Mining added and now plots correctly

--- week4/task_b.py
+++ week4/task_b.py
@@ -6,15 +6,21 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import pytorch_metric_learning.utils.logging_presets as logging_presets
+import matplotlib
 
 from cycler import cycler
-from pytorch_metric_learning import testers, samplers, losses, distances, trainers
+from pytorch_metric_learning import (
+    testers, samplers, losses, distances, trainers, miners)
 from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator
 
 from src.utils import get_configuration
 from src.datasets.mit_split import create_mit_dataloader
 from src.models.resnet import ResNetWithEmbedder
 from src.models.vgg import VGG19
+
+
+OUTPUT_PATH = './outputs'
+EXPERIMENT_NAME = None
 
 
 def __parse_args() -> argparse.Namespace:
@@ -35,7 +41,7 @@ def __parse_args() -> argparse.Namespace:
     # Model configuration
     parser.add_argument('--model', type=str, default='resnet_18',
                         help='Model to use. Options: resnet_X, vgg.')
-    parser.add_argument('--embedding_size', type=int, default=128,
+    parser.add_argument('--embedding_size', type=int, default=256,
                         help='Size of the embedding vector.')
     # Loss configuration
     parser.add_argument('--loss', type=str, default='contrastive',
@@ -46,6 +52,21 @@ def __parse_args() -> argparse.Namespace:
                         help='Negative margin for contrastive loss.')
     parser.add_argument('--distance', type=str, default='euclidean',
                         help='Distance to use. Options: euclidean, cosine.')
+    # Miner configuration
+    parser.add_argument('--miner', type=str, default="PairMargin",
+                        help='Miner to use. Options: BatchEasyHard, BatchHard, MultiSimilarity, PairMargin, TripletMargin.')
+    parser.add_argument('--miner_pos_strategy', type=str, default='easy',
+                        help='Positive strategy for the miner. Options: all, easy, semihard, hard.')
+    parser.add_argument('--miner_neg_strategy', type=str, default='hard',
+                        help='Negative strategy for the miner. Options: all, easy, semihard, hard.')
+    parser.add_argument('--miner_epsilon', type=float, default=0.1,
+                        help='Epsilon for the miner.')
+    parser.add_argument('--miner_pos_margin', type=float, default=0.2,
+                        help='Positive margin for the miner.')
+    parser.add_argument('--miner_neg_margin', type=float, default=0.8,
+                        help='Negative margin for the miner.')
+    parser.add_argument('--miner_type_of_triplets', type=str, default='all',
+                        help='Type of triplets for the miner. Options: all, easy, semihard, hard.')
     # Training configuration
     parser.add_argument('--optimizer', type=str, default='adam',
                         help='Optimizer to use. Options: adam, sgd.')
@@ -62,9 +83,13 @@ def __parse_args() -> argparse.Namespace:
     return args
 
 
-def visualizer_hook(umapper, umap_embeddings, labels, split_name, keyname, output_path="./outputs", *args):
+def visualizer_hook(umapper, umap_embeddings, labels, split_name, keyname, epoch, *args):
+    global OUTPUT_PATH, EXPERIMENT_NAME
+    output_dir = os.path.join(OUTPUT_PATH, "umap_plots")
+    os.makedirs(output_dir, exist_ok=True)
     logging.info(
-        "UMAP plot for the {} split and label set {}".format(split_name, keyname)
+        "UMAP plot for the {} split and label set {}".format(
+            split_name, keyname)
     )
     label_set = np.unique(labels)
     num_classes = len(label_set)
@@ -80,12 +105,15 @@ def visualizer_hook(umapper, umap_embeddings, labels, split_name, keyname, outpu
         plt.plot(umap_embeddings[idx, 0],
                  umap_embeddings[idx, 1], ".", markersize=1)
     plt.show()
-    fig.savefig(os.path.join(output_path, f"umap_{split_name}_{keyname}.png"))
+    fig.savefig(os.path.join(output_dir, f"umap_{split_name}_{epoch}_{EXPERIMENT_NAME}.png"))
 
 
 def main(args: argparse.Namespace):
+    global OUTPUT_PATH, EXPERIMENT_NAME
     os.makedirs(args.output_path, exist_ok=True)
+    OUTPUT_PATH = args.output_path
     experiment_name = f"{args.model}_{args.dataset}_loss_{args.loss}_distance_{args.distance}_posmargin_{args.pos_margin}_negmargin_{args.neg_margin}"
+    EXPERIMENT_NAME = experiment_name
     model_folder = os.path.join(args.output_path, "models")
     os.makedirs(model_folder, exist_ok=True)
     logs_folder = os.path.join(args.output_path, "logs", experiment_name)
@@ -97,7 +125,8 @@ def main(args: argparse.Namespace):
 
     # Model loading
     if args.model.split("_")[0] == 'resnet':
-        model = ResNetWithEmbedder(resnet=args.model.split("_")[1], embed_size=args.embedding_size)
+        model = ResNetWithEmbedder(resnet=args.model.split("_")[
+                                   1], embed_size=args.embedding_size)
     elif args.model == 'vgg':
         model = VGG19()
     else:
@@ -117,7 +146,7 @@ def main(args: argparse.Namespace):
         raise NotImplementedError
     else:
         raise ValueError(f'Unknown dataset: {args.dataset}')
-    
+
     class_sampler = samplers.MPerClassSampler(
         labels=train_ds.targets,
         m=args.batch_size // 8,
@@ -147,13 +176,39 @@ def main(args: argparse.Namespace):
     else:
         raise ValueError(f'Unknown loss: {args.loss}')
 
+    # Miner configuration
+    if args.miner == "BatchEasyHard":
+        miner = miners.BatchEasyHardMiner(
+            pos_strategy=args.miner_pos_strategy,
+            neg_strategy=args.miner_neg_strategy,
+        )
+    elif args.miner == "BatchHard":
+        miner = miners.BatchHardMiner()
+    elif args.miner == "MultiSimilarity":
+        miner = miners.MultiSimilarityMiner(
+            epsilon=args.miner_epsilon,
+        )
+    elif args.miner == "PairMargin":
+        miner = miners.PairMarginMiner(
+            pos_margin=args.miner_pos_margin,
+            neg_margin=args.miner_neg_margin,
+        )
+    elif args.miner == "TripletMargin":
+        miner = miners.TripletMarginMiner(
+            margin=args.miner_pos_margin,
+            type_of_triplets=args.miner_type_of_triplets,
+        )
+
     # Optimizer configuration
     if args.optimizer == 'adam':
-        trunk_optimizer = torch.optim.Adam(model.trunk.parameters(), lr=args.lr_trunk)
-        embedder_optimizer = torch.optim.Adam(model.embedder.parameters(), lr=args.lr_embedder)
+        trunk_optimizer = torch.optim.Adam(
+            model.trunk.parameters(), lr=args.lr_trunk)
+        embedder_optimizer = torch.optim.Adam(
+            model.embedder.parameters(), lr=args.lr_embedder)
     elif args.optimizer == 'sgd':
         trunk_optimizer = torch.optim.SGD(model.parameters(), lr=args.lr_trunk)
-        embedder_optimizer = torch.optim.SGD(model.parameters(), lr=args.lr_embedder)
+        embedder_optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.lr_embedder)
     else:
         raise ValueError(f'Unknown optimizer: {args.optimizer}')
 
@@ -172,21 +227,23 @@ def main(args: argparse.Namespace):
     )
 
     end_of_epoch_hook = hooks.end_of_epoch_hook(
-        tester, 
-        {"val": val_ds}, 
-        model_folder, 
-        test_interval=1, 
+        tester,
+        {"val": val_ds},
+        model_folder,
+        test_interval=1,
         patience=1,
     )
 
     # Training
     trainer = trainers.MetricLossOnly(
         models={"trunk": model.trunk, "embedder": model.embedder},
-        optimizers={"trunk_optimizer": trunk_optimizer, "embedder_optimizer": embedder_optimizer},
+        optimizers={"trunk_optimizer": trunk_optimizer,
+                    "embedder_optimizer": embedder_optimizer},
         loss_funcs={"metric_loss": criterion},
-        batch_size=args.batch_size,
-        dataset=train_ds,
+        mining_funcs={"tuple_miner": miner},
         data_device=device,
+        dataset=train_ds,
+        batch_size=args.batch_size,
         sampler=class_sampler,
         end_of_iteration_hook=hooks.end_of_iteration_hook,
         end_of_epoch_hook=end_of_epoch_hook,
@@ -200,5 +257,6 @@ def main(args: argparse.Namespace):
 
 
 if __name__ == "__main__":
+    matplotlib.use('Agg')
     args = __parse_args()
     main(args)
