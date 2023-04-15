a369ef947293621876d51a4e88b83653f53a60df
AtenrevCode <atenrev@gmail.com>
2023-04-16 01:51:19 +0200

    Slurm execution scripts

---
+++ week4/run_task_b_experiments.sh
@@ -0,0 +1,6 @@
+sbatch ./empress_contrastive_multisimilarity.slurm
+sbatch ./empress_contrastive_none.slurm
+sbatch ./empress_contrastive_pairmargin.slurm
+sbatch ./empress_ntxent_multisimilarity.slurm
+sbatch ./empress_ntxent_none.slurm
+sbatch ./empress_ntxent_pairmargin.slurm
---
+++ week4/run_task_c_experiments.sh
@@ -0,0 +1,4 @@
+sbatch ./empress_triplet_multisimilarity.slurm
+sbatch ./empress_triplet_none.slurm
+sbatch ./empress_triplet_tripletmargin.slurm
+sbatch ./empress_triplet_batchhard.slurm
