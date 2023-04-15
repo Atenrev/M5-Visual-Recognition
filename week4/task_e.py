58b993f0e7833a147441b577579da47ec2bbaf2c
Alex <21alexth@gmail.com>
2023-04-15 17:52:50 +0200

    save model every 5 epoch

--- week4/task_e.py
+++ week4/task_e.py
@@ -239,10 +239,15 @@ def main(args: argparse.Namespace):
             running_loss.append(loss.cpu().detach().numpy())
         logging.info("Epoch: {}/{} - Loss: {:.4f}".format(epoch + 1, epochs, np.mean(running_loss)))
 
+        # Save model every 5 epochs
+        if (epoch + 1) % 5 == 0:
+            torch.save(model.state_dict(), os.path.join(
+                model_folder, f'model_{epoch + 1}.pth'))
+            logging.info(f"Model saved at {model_folder}")
+
     logging.info("Training finished!")
 
     # Save model
-    os.makedirs('./models', exist_ok=True)
     torch.save(model.state_dict(), os.path.join(
         model_folder, f'model_final.pth'))
     logging.info(f"Model saved at {model_folder}")
