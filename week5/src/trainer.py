b108890724a320bb270f30b45c008f9e29dd6eeb
AtenrevCode <atenrev@gmail.com>
2023-04-20 01:06:32 +0200

    A bit of reformat

--- week5/src/trainer.py
+++ week5/src/trainer.py
@@ -19,7 +19,31 @@ class Runner:
         self.run_count = 0
         self.reset()
 
+    def calculate_accuracy(self, anchor_embeddings, positive_embeddings, batch_size, negative_embeddings=None):
+        if negative_embeddings is not None:
+            positive_distances = torch.sum(
+                (anchor_embeddings - positive_embeddings) ** 2, dim=1)
+            negative_distances = torch.sum(
+                (anchor_embeddings - negative_embeddings) ** 2, dim=1)
+            accuracy = torch.mean(
+                (positive_distances < negative_distances).float()).item()
+            self.metrics['accuracy'].update(accuracy)
+        else:
+            # Compute pairwise distances
+            distances = np.dot(anchor_embeddings.detach().cpu().numpy(), positive_embeddings.T.detach().cpu().numpy())
+            distances = 1 - distances  # convert dot product to cosine distance
+
+            # Find most similar embeddings
+            anchor_to_positive = np.argmin(distances, axis=1)
+            positive_to_anchor = np.argmin(distances, axis=0)
+
+            # Compute accuracy score
+            correct_matches = np.sum(np.arange(batch_size) == anchor_to_positive[positive_to_anchor])
+            accuracy = correct_matches / batch_size
+            self.metrics['accuracy'].update(accuracy)
+
     def run_epoch(self, dataloader):
+        # TODO: handle image_to_text, text_to_image and symmetric
         if self.train:
             self.model.train()
         else:
@@ -41,10 +65,12 @@ class Runner:
                 )
                 loss = self.loss_fn(anchor_embeddings,
                             positive_embeddings, negative_embeddings)
+                self.calculate_accuracy(anchor_embeddings, positive_embeddings, batch_size, negative_embeddings)
             else:
                 logits, image_embeddings, text_embeddings = self.model(
                     anchors, positives.input_ids, positives.attention_mask)
                 loss = self.loss_fn(logits)
+                self.calculate_accuracy(image_embeddings, text_embeddings, batch_size)
 
             # Backward
             if self.train:
@@ -53,32 +79,8 @@ class Runner:
                 self.optimizer.step()
 
             # Metrics
-            self.metrics['loss'].update(loss.item())
+            self.metrics['loss'].update(loss.item())                
 
-            # Calculate accuracy
-            if isinstance(self.model, TripletModel):
-                positive_distances = torch.sum(
-                    (anchor_embeddings - positive_embeddings) ** 2, dim=1)
-                negative_distances = torch.sum(
-                    (anchor_embeddings - negative_embeddings) ** 2, dim=1)
-                accuracy = torch.mean(
-                    (positive_distances < negative_distances).float()).item()
-                self.metrics['accuracy'].update(accuracy)
-            else:
-                # Compute pairwise distances
-                distances = np.dot(image_embeddings.detach().cpu().numpy(), text_embeddings.T.detach().cpu().numpy())
-                distances = 1 - distances  # convert dot product to cosine distance
-
-                # Find most similar embeddings
-                image_to_text = np.argmin(distances, axis=1)
-                text_to_image = np.argmin(distances, axis=0)
-
-                # Compute accuracy score
-                correct_matches = np.sum(np.arange(batch_size) == image_to_text[text_to_image])
-                accuracy = correct_matches / batch_size
-                self.metrics['accuracy'].update(accuracy)
-
-            # Log metrics
             if self.tracker is not None:
                 for i, metric in enumerate(self.metrics.values()):
                     self.tracker.add_batch_metric(
@@ -127,7 +129,7 @@ def train(train_dataloader, val_dataloader, model, loss_fn, optimizer, device, e
                 optimizer
             )
 
-        summary = f"Epoch {epoch}/{epochs} - Train loss: {train_runner.metrics['loss'].average:.4f} - Val loss: {val_runner.metrics['loss'].average:.4f}"
+        summary = f"Epoch {epoch}/{epochs} - Train loss: {train_runner.metrics['loss'].average:.4f} - Val loss: {val_runner.metrics['loss'].average:.4f} - Train accuracy: {train_runner.metrics['accuracy'].average:.4f} - Val accuracy: {val_runner.metrics['accuracy'].average:.4f}"
         print("\n", summary, "\n")
 
         train_runner.reset()
